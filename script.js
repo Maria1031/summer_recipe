b0156368b7031b9e451c890a09e2d9a3e063a2f7
Maria1031 <marimiona1015@gmail.com>
2024-07-31 10:54:30 +0900

    initial commit.
    練習です。

---
+++ script.js
@@ -0,0 +1,26 @@
+let nav = document.querySelector("#navArea");
+let btn = document.querySelector(".toggle-btn");
+let mask = document.querySelector("#mask");
+let concept = document.querySelector(".concept");
+let pork = document.querySelector(".pork");
+let recipe1 = document.querySelector(".recipe1");
+
+btn.onclick = () =>{
+    nav.classList.toggle("open");
+};
+
+mask.onclick = () =>{
+    nav.classList.toggle("open");
+};
+
+concept.onclick = () =>{
+    nav.classList.toggle("open");
+};
+
+pork.onclick = () =>{
+    nav.classList.toggle("open");
+};
+
+recipe1.onclick = () =>{
+    nav.classList.toggle("open");
+};
